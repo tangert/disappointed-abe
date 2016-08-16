80cc9e112d14d64fe5f142d2a834ccff219f78d8
tangert <tyler@angert.com>
2016-08-16 18:53:23 -0400

    generic autoreply script added

--- genericAutoReply.py
+++ genericAutoReply.py
@@ -7,27 +7,18 @@ from tweepy import OAuthHandler
 from tweepy import API
 from tweepy.streaming import StreamListener
 
-ckey = 'JUo1JwXJXNuFOmbrjboKC7Iz9'
-csecret = 'zFDQKK4vcdlIuIJJH6453iJgGlVpRYgg4sws1MKORZG7b0LXWE'
-atoken = '762753721986060288-YQPemCSDa4w1c3q7tw4YFmG6xf6i52B'
-asecret = '2wNgl0ezFLqJu0dswE3ukmNySxl2LlFj6J7PosTiVUlHf'
+ckey = ''
+csecret = ''
+atoken = ''
+asecret = ''
 
-class trumpListener(StreamListener):
-
-	#trumpID = '25073877'
+class autoReply(StreamListener):
 
 	auth = OAuthHandler(ckey, csecret)
 	auth.set_access_token(atoken, asecret)
 
-	#header = "Dear Mr. Trump,"
-	footer = "Sincerely, a Dissapointed Abe Lincoln."
-	screenName = 'DisappointedAbe'
-	
-	responses = [
-	"I died for THIS?!",
-	"If I was alive today, I would shoot myself just because of you.",
-	"I remember when the GOP actually had politicans in it. Guess not anymore."
-	]
+	screenName = ''
+	response = ''
 
 	def on_connect(self):
 		print "Internet ok."
@@ -35,12 +26,11 @@ class trumpListener(StreamListener):
 	def on_data(self, raw_data):
 
 		api = API(self.auth)
-		user = api.get_user(screen_name = 'realDonaldTrump')
+		user = api.get_user(screen_name = self.screenName)
 		tweetID = user.id
 
-		newTweet = api.user_timeline(screen_name = 'DisappointedAbe',count=10)[0]
-		oldTweet = newTweet #storage
-		replyText = '@realDonaldTrump ' + self.responses[responseNumber] + ' ' + self.footer
+		newTweet = api.user_timeline(screen_name = self.screenName,count=10)[0]
+		replyText = '@' + screenName + ' ' + response
 
 		#checking if tweet is longer than 140 (but shouldn't happen)
 		if len(replyText) > 140:
